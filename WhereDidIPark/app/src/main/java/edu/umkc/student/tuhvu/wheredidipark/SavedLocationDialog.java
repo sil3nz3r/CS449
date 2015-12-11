d86fcb5ca266bfd05fae1161008bb40d93f118ea
Sil3nz3r <tuvu23061992@gmail.com>
2015-12-11 10:56:22 -0600

    Some work on the ListView concept of the Location Dialog

--- WhereDidIPark/app/src/main/java/edu/umkc/student/tuhvu/wheredidipark/SavedLocationDialog.java
+++ WhereDidIPark/app/src/main/java/edu/umkc/student/tuhvu/wheredidipark/SavedLocationDialog.java
@@ -9,7 +9,18 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sil3nz3r on 12/01/2015.
@@ -22,13 +33,44 @@ public class SavedLocationDialog extends DialogFragment {
         mLocationSharedPreferences = getActivity().getSharedPreferences(getString(R.string.location_shared_preferences_name), Context.MODE_PRIVATE);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Saved Location")
-                .setItems(new String[]{"Location 1", "Location 2", "Location 3"},
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int index) {
-                                retrieveLocationFromSharedPreferences(index);
-                            }
-                        });
+//        builder.setTitle("Saved Location");
+//                .setItems(new String[]{"Location 1", "Location 2", "Location 3"},
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int index) {
+//                                retrieveLocationFromSharedPreferences(index);
+//                            }
+//                        });
+
+        // Custom ListView in Java
+        ListView list = new ListView(getActivity());
+        List<String> elements = new ArrayList<String>();
+        for (int i = 0; i < 3; i ++) {
+            elements.add("Location" + String.valueOf(i + 1));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_2, android.R.id.text1, elements);
+
+        //ListAdapter adapter1 = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, mCursor,)
+
+        list.setAdapter(adapter);
+        //list.setOnItemClickListener();
+        builder.setView(list);
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder//.setView(inflater.inflate(R.layout.dialog, null))
+                .setTitle("Saved Location")
+                .setView(list)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // do something ...
+                    }
+                });
         return builder.create();
     }
 
